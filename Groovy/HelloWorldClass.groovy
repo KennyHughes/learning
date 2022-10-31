438fe25b349239e8bd47d16f41e08aee2c1b4e70
Kenny Hughes <kenhughes@linkedin.com>
2022-10-31 16:20:52 -0600

    static and dynamic typing in groovy

--- Groovy/HelloWorldClass.groovy
+++ Groovy/HelloWorldClass.groovy
@@ -1,5 +1,17 @@
 class HelloWorld {
     static void main(String[] args) {
         println "Hello World"
+
+        int age = 40
+
+        println age
+
+        println age.getClass()
+
+        def name = "John"
+
+        println name
+
+        println name.getClass()
     }
 }
